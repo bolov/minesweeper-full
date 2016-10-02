b4ff260e32753145b1d1ff849f4011bd58906b77
Mihail Balan <mihail.balan@gmail.com>
2016-10-02 04:41:58 +0300

    some public to private

--- Minesweeper Full/grid.h
+++ Minesweeper Full/grid.h
@@ -24,7 +24,6 @@ public:
     static const int sk_bomb_cell = -1;
 
 private:
-public: // TODO delete
     bolov::containers::Matrix<int> bombs_;
     bolov::containers::Matrix<Display> display_;
     bolov::gslx::size_t num_bombs_;
@@ -77,6 +76,9 @@ public:
         Ensures(bombs_.column_size() == display_.column_size());
         Ensures(bombs_.line_size() == display_.line_size());
     }
+
+    const auto& bombs() const { return bombs_; }
+    const auto& display() const { return display_; }
 };
 
 inline auto operator<<(std::ostream& os, const Grid& grid) -> std::ostream&
@@ -84,26 +86,26 @@ inline auto operator<<(std::ostream& os, const Grid& grid) -> std::ostream&
     using bolov::utils::Size_range;
 
     os << "   ";
-    for (auto i : Size_range{0, grid.bombs_.line_size()})
+    for (auto i : Size_range{0, grid.bombs().line_size()})
         os << (i % 10) << " ";
     os << std::endl;
     os << "   ";
-    for (auto i : Size_range{0, grid.bombs_.line_size()})
+    for (auto i : Size_range{0, grid.bombs().line_size()})
         os << "- ";
     os << std::endl << std::endl;
 
-    for (auto i : Size_range{0, grid.bombs_.column_size()}) {
+    for (auto i : Size_range{0, grid.bombs().column_size()}) {
         os << (i % 10) << "| ";
-        for (auto j : Size_range{0, grid.bombs_.line_size()}) {
-            switch (grid.display_[i][j])
+        for (auto j : Size_range{0, grid.bombs().line_size()}) {
+            switch (grid.display()[i][j])
             {
             case Grid::Display::e_shown:
-                if (grid.bombs_[i][j] == Grid::sk_bomb_cell)
+                if (grid.bombs()[i][j] == Grid::sk_bomb_cell)
                     os << '*' << " ";
-                else if (grid.bombs_[i][j] == 0)
+                else if (grid.bombs()[i][j] == 0)
                     os << ' ' << " ";
                 else
-                    os << grid.bombs_[i][j] << " ";
+                    os << grid.bombs()[i][j] << " ";
                 break;
             case Grid::Display::e_hidden:
                 os << '\"' << " ";
